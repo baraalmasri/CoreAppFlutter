5dd4ead9ba05d47462efce5567641bcaaff5bb9d
Baraa Masri <15340030+baraalmasri@users.noreply.github.com>
2022-09-18 00:50:31 +0300

    svg images added with their paths

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,5 +2,9 @@ const String IMAGE_PATH = "assets/images";
 
 class ImageAssets{
   static const String splashLogo ="$IMAGE_PATH/splash_logo.png";
+  static const String onboardingHomeSetLogo ="$IMAGE_PATH/onboarding_logo_home_set.svg";
+  static const String onboardingHouseLogo ="$IMAGE_PATH/onboarding_logo_house.svg";
+  static const String onboardingCarLogo ="$IMAGE_PATH/onboarding_logo_car.svg";
+  static const String onboardingElecitronicLogo ="$IMAGE_PATH/onboarding_logo_elecitronic.svg";
 
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -3,4 +3,13 @@
 
 class AppStrings{
   static const noRouteFound=" No Route Found";
+  static const onBoardingTitle1="SEE THE BEST COURSE #1";
+  static const onBoardingTitle2="SEE THE BEST COURSE #2";
+  static const onBoardingTitle3="SEE THE BEST COURSE #3";
+  static const onBoardingTitle4="SEE THE BEST COURSE #4";
+
+  static const onBoardingSubTitle1="this app is an awsome flutter application using clean architecture  #1";
+  static const onBoardingSubTitle2="this app is an awsome flutter application using clean architecture  #2";
+  static const onBoardingSubTitle3="this app is an awsome flutter application using clean architecture  #3";
+  static const onBoardingSubTitle4="this app is an awsome flutter application using clean architecture  #4";
 }
