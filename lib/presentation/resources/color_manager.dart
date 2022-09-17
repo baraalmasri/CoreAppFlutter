d04efd9e6bbc38a7d9ff336384be07cc7807e2a3
Baraa Masri <15340030+baraalmasri@users.noreply.github.com>
2022-09-17 03:02:05 +0300

    the photos  added

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -26,4 +26,34 @@ class ColorManager{
   static Color white =const Color (0xffFFFFFF);
   static Color error =const Color (0xffe61f34);
 
+  // my sexy colors
+  //-1
+  static Color coffeeChocolate  =const Color (0xff924936);
+  static Color karamile =const Color (0xffDC853D);
+  static Color strawberry =const Color (0xffDC4981);
+//-2
+  static Color leafGreen =const Color (0xff165954);
+  static Color beige =const Color (0xffF9AC66);
+  static Color orangeJuice =const Color (0xffED6B5B);
+
+//-3
+  static Color bananaYellow =const Color (0xffFDAE38);
+  static Color terkuaz =const Color (0xff5FA3A5);
+  static Color darkGreen =const Color (0xff264248);
+
+  //-4
+  static Color talaColor =const Color (0xff876D97);
+  static Color beige_ =const Color (0xffF9AC66);
+  static Color pinkOrange=const Color (0xffFF8381);
+
+
+  //-5
+  static Color greenJungle =const Color (0xff5D743C);
+  static Color eggYellow =const Color (0xffFFB955);
+  static Color darkGreenJungle=const Color (0xff1F4E48);
+
+  // perfect gray
+  static Color perfectGray=const Color (0xff444444);
+  //perfect Tirkwaz
+  static Color Perfect =const Color (0xff00d1cd);
 }
