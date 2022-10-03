f9baf9515632699f37c02c244001528f2912baaa
Baraa Masri <15340030+baraalmasri@users.noreply.github.com>
2022-10-03 21:24:37 +0300

    error handler class code added

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -14,3 +14,25 @@ enum DataSoruce{
   CACHE_ERROR,
   NO_INTERNET_CONNECTION
 }
+class ResponseCode{
+  //api /server side status codes
+  static const int SUCCESS=200;
+  static const int NO_CONTENT=201;
+  static const int BAD_REQUEST=400;
+  static const int FORBIDDEN=403;
+  static const int UNAUTORISED=401;
+  static const int NOT_FOUND=404;
+  static const int INTERNAL_SERVER_ERROR=500;
+
+
+  //local status codes
+  static const int CONNECT_TIMEOUT=-1;
+  static const int CANCEL=-2;
+  static const int RECIEVE_TIMEOUT=-3;
+  static const int SEND_TIMEOUT=-4;
+  static const int CACHE_ERROR=-5;
+  static const int NO_INTERNET_CONNECTION=-6;
+  static const int unknown=-7;
+
+
+}
